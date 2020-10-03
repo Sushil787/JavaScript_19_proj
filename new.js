5167d4223d14440d92274dc0ee0f6469316af857
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2020-10-03 16:54:23 +0545

    Create new.js

---
+++ new.js
@@ -0,0 +1,15 @@
+const firstName = "Sushil";
+const lastName = "gyawali";
+const sex = "male";
+const age = 19;
+const email = "itsushil100@outlook.com";
+const password = "******";
+const isLoggedInFrom = "Github";
+console.log(`
+"user name is " : ${firstName} + ${lastName}
+"sex": ${sex}
+"age": ${age}
+"with email": ${email}
+"with password" : ${password}
+"and is Logged in from" : ${isLoggedInFrom}
+`)
