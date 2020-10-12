93b02310403fef036c6f071b74cba00675c8b895
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2020-10-12 20:53:31 +0545

    Create function.js

---
+++ function.js
@@ -0,0 +1,14 @@
+let userName = 'Sushil';
+
+function showMessage() {
+  userName = "surendra"; 
+
+  let message = 'Hello, ' + userName;
+  alert(message);
+}
+
+alert( userName ); 
+
+showMessage();
+
+alert( userName ); 
